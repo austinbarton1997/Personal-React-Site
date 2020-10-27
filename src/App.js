43fa11cc9622bdc7684060a1509ff2c5ad4049e3
Austin Barton <mail@austinbarton.dev>
2020-10-27 10:46:54 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
-import Navbar from "./components/Navbar"
-import Introduction from "./components/Introduction"
+import React from "react";
+import Navbar from "./components/Navbar";
+import Introduction from "./components/Introduction";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Left from './components/Left';
-import Right from './components/Right'
-import Contact from './components/Contact'
+import Right from './components/Right';
+import Contact from './components/Contact';
 
 function App() {
   return (
