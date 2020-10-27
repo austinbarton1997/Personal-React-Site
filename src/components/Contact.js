7c2e606533c4b2245244ee9d7ed7c75a1de2dab1
Austin Barton <mail@austinbarton.dev>
2020-10-27 10:57:47 -0600

    Navbar link fixes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,8 +4,8 @@ export default function Contact() {
     return (
         <div class="jumbotron">
             <div class="container contactView">
-                <p class="num3 contact" id="about"> What's Next?<hr /></p>
-                <h1 class="contact_me" id="about">Get In Touch</h1>
+                <p class="num3 contact" id="contact"> What's Next?<hr /></p>
+                <h1 class="contact_me">Get In Touch</h1>
                 <p class="contactParagraph">I am actively looking for new opportunities and open to remote work or relocation. I would love to hear from you!</p>
                 <div class="text-center">
                     <a href="mailto:mail@austinbarton.dev" class="btn btn-outline-light contact_btn">Say Hello</a>
