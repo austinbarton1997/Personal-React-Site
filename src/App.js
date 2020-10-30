45a4cc8549549cbf985d365266796db10d823509
Austin Barton <mail@austinbarton.dev>
2020-10-30 08:11:11 -0600

    Refactoring

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import './App.css';
+import './stylesheets/App.css';
+import './stylesheets/About.css';
+import './stylesheets/Contact.css';
+import './stylesheets/Introduction.css';
+import './stylesheets/Left.css';
+import './stylesheets/Right.css';
+import './stylesheets/Navbar.css';
+import './stylesheets/Projects.css';
 import React from "react";
-import Navbar from "./components/Navbar";
-import Introduction from "./components/Introduction";
-import About from "./components/About";
-import Projects from "./components/Projects";
-import Left from './components/Left';
-import Right from './components/Right';
-import Contact from './components/Contact';
+import { Navbar, Introduction, About, Projects, Left, Right, Contact } from "./components/";
 
 function App() {
   return (
--- src/components/Left.js
+++ src/components/Left.js
@@ -2,7 +2,7 @@ import React from "react"
 
 export default function Left() {
     return (
-        <div class="dxVzdv verticalLine">
+        <div class="lineLeft verticalLine">
             <a href="http://www.github.com/austinbarton1997"><i class="fab fa-github github"></i></a>
             <a href="https://www.linkedin.com/"><i class="fab fa-linkedin-in linkedin"></i></a>
         </div>
