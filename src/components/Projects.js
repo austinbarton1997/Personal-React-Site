aff52254bc378f736856a9ad015fbb3f101f2fc5
Austin Barton <mail@austinbarton.dev>
2020-10-30 08:19:47 -0600

    Link Updates

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,7 +8,7 @@ export default function Projects() {
                 <li class="media">
                     <div class="media-body">
                         <h5 class="mt-0 mb-1"><p class="featuredText1">Featured Project</p>
-                            <h4 class="featuredTitle1">Notes App</h4></h5>
+                            <h4 class="featuredTitle1"><a href="https://github.com/austinbarton1997/Notes">Notes App</a></h4></h5>
                         <p class="project-paragraph1">A platform built for your notes, styled after apple notes.
                         Keep track of notes and things you need to get done! Delete the note when you're done with a simple click.</p>
                     </div>
@@ -19,7 +19,7 @@ export default function Projects() {
                 <li class="media">
                     <div class="media-body">
                         <h5 class="mt-0 mb-1"><p class="featuredText1">Featured Project</p>
-                            <h4 class="featuredTitle1">Tinder Movies</h4></h5>
+                            <h4 class="featuredTitle1"><a href="https://github.com/austinbarton1997/Tinder-Movies">Tinder Movies</a></h4></h5>
                         <p class="project-paragraph1">A platform built for you and your partner.
                         Movies that appear on netflix, like or dislike the movie. The movie will be stored in a list. If your
                      partner happens to like the same movie you will get an alert!</p>
