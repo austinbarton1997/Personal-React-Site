7e217af6c3bcfb9dd5ef136ef7f6e8f080297c91
Austin <mail@austinbarton.dev>
2020-11-02 17:05:52 -0700

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ export default function About() {
             <div class="container">
                 <h1 class="about-line num1 about-me" id="about">About Me<hr /></h1>
                 <p class="about-paragraph">Hello! I'm Austin, a software engineer currently based in Denver, CO.</p>
-                <p class="about-paragraph">My background is in food management where I gained a passion for delivering a superior customer experience. This passion has evolved into me being obsessed with the user experience and has helped me develop skils to create software that is both beautiful and intuitive.</p>
+                <p class="about-paragraph">My background is in food management where I gained a passion for delivering a superior customer experience. This passion has evolved into me being obsessed with the user experience and has helped me develop skills to create software that is both beautiful and intuitive.</p>
                 <p class="about-paragraph">I enjoy learning about new and interesting technologies and combing through documentation to pick up those skills quickly.</p>
                 <p class="about-paragraph">Here are a few technologies I've been working with recently:</p>
                 <ul class="about_skills">
