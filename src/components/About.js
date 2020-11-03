16c0fda3461516dc1cf86076e6e0272084372bfb
Austin <mail@austinbarton.dev>
2020-11-02 17:21:12 -0700

    Size Fixes

--- src/components/About.js
+++ src/components/About.js
@@ -6,10 +6,12 @@ export default function About() {
         <div class="jumbotron">
             <div class="container align">
                 <h1 class="about-line num1 about-me" id="about">About Me<hr /></h1>
-                <p class="about-paragraph">Hello! I'm Austin, a software engineer currently based in Denver, CO.</p>
-                <p class="about-paragraph">My background is in food management where I gained a passion for delivering a superior customer experience. This passion has evolved into me being obsessed with the user experience and has helped me develop skills to create software that is both beautiful and intuitive.</p>
-                <p class="about-paragraph">I enjoy learning about new and interesting technologies and combing through documentation to pick up those skills quickly.</p>
-                <p class="about-paragraph">Here are a few technologies I've been working with recently:</p>
+                <div>
+                  <p class="about-paragraph">Hello! I'm Austin, a software engineer currently based in Denver, CO.</p>
+                  <p class="about-paragraph">My background is in food management where I gained a passion for delivering a superior customer experience. This passion has evolved into me being obsessed with the user experience and has helped me develop skills to create software that is both beautiful and intuitive.</p>
+                  <p class="about-paragraph">I enjoy learning about new and interesting technologies and combing through documentation to pick up those skills quickly.</p>
+                  <p class="about-paragraph">Here are a few technologies I've been working with recently:</p>
+                </div>
                 <ul class="about_skills">
                     <li class="about_skills currentSkills">JavaScript (ES6+)</li>
                     <li class="about_skills currentSkills">Python</li>
